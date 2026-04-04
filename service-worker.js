fe85587253585b419b448c4821a04283b40a6922
bravoyasociados <huertare2012@gmail.com>
2026-04-04 00:55:59 -0600

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,21 @@
-const CACHE_NAME = "King's Barber-v1";
+const CACHE_NAME = "barber-cache-v1";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./logo.png"
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
   );
 });
